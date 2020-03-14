fea3139841e8f4ab2c975ad6691e1dcc75f63273
pankajwithgit <pankaj.davim@gmail.com>
2020-03-14 10:08:13 -0400

    Update TopKFrequentElements.java

--- array/medium/TopKFrequentElements.java
+++ array/medium/TopKFrequentElements.java
@@ -1,9 +1,10 @@
+// Overall O(nlogk) time complexity and o(n) space complexity.
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
         // Base case.
         if(nums == null) return null;
         
-        // Build the frequency map first.
+        // Build the frequency map first. It will take O(n) considering n is number of elements in array.
         Map<Integer, Integer> freqMap = new HashMap<>();
         for(int i = 0; i < nums.length; i++) {
             freqMap.put(nums[i], freqMap.getOrDefault(nums[i], 0) + 1);
@@ -18,7 +19,7 @@ class Solution {
             }
         };
         
-        // Build a priority queue to keep k elements. Priority queue will be ordered by smallest frequency first, so keep pushing out element to just maintain k elements only.
+        // Build a priority queue to keep k elements. Priority queue will be ordered by smallest frequency first, so keep pushing out element to just maintain k elements only. It will take O(nlogk) time.
         PriorityQueue<Integer> q = new PriorityQueue<>(freqComparator);
         for(Integer n : freqMap.keySet()) {
             q.add(n);
