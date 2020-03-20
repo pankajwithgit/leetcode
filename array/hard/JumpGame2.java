b4a39bb7017e24c07fc217cc2e3f697a775c5973
pankajwithgit <pankaj.davim@gmail.com>
2020-03-20 18:46:55 -0400

    https://leetcode.com/problems/jump-game-ii/
    https://leetcode.com/problems/jump-game-ii/

---
+++ array/hard/JumpGame2.java
@@ -0,0 +1,29 @@
+// It's a DP problem. We will continue to maintain and use min jumps required to reach
+// each element and ultimately find out steps to reach last index. 
+class Solution {
+    public int jump(int[] nums) {
+        if(nums == null) return 0;
+        
+        int len = nums.length;
+        if(len < 2) return 0;
+
+        // We need to prepare a parallel array to maintain min jumps required to reach each element.
+        int[] minJumps = new int[len];
+        
+        for(int i = 1; i < len; i++) {
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < i; j++) {
+                if(isInRange(nums[j], j, i)) {
+                    min = Math.min(min, minJumps[j] + 1);
+                }
+            }
+            minJumps[i] = min;
+        }
+        return minJumps[len - 1];
+    }
+    
+    // To check and see if current index can be reached from a particular index or not.
+    private boolean isInRange(int fromElement, int fromIndex, int targetIndex) {
+        return fromIndex + fromElement >= targetIndex;
+    }
+}
