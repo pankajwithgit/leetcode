8ac647f389f1a7dcd62de638f7769acfc7b6ea69
pankajwithgit <pankaj.davim@gmail.com>
2020-03-28 19:04:01 -0400

    https://leetcode.com/problems/combination-sum/
    https://leetcode.com/problems/combination-sum/

---
+++ array/medium/CombinationSum.java
@@ -0,0 +1,17 @@
+class Solution {
+    List<List<Integer>> sums = new LinkedList();
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        backtrack(candidates, new LinkedList(), 0, target);
+        return sums;
+    }
+    
+    public void backtrack(int[] candidates, LinkedList<Integer> sum, int start, int remain){
+        if(remain < 0) return;
+        if(remain == 0){ sums.add(new LinkedList(sum)); return; }
+        for(int i=start; i<candidates.length; i++){
+            sum.add(candidates[i]);
+            backtrack(candidates, sum, i, remain-candidates[i]);
+            sum.remove(sum.size()-1);
+        }
+    } 
+}
