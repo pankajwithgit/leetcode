84c25ab2f4ec1855bf26ccfcc89456b88eeee9ec
pankajwithgit <pankaj.davim@gmail.com>
2020-04-19 11:09:14 -0400

    https://leetcode.com/problems/plus-one/
    https://leetcode.com/problems/plus-one/

---
+++ array/easy/PlusOne.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for(int i = len - 1; i >= 0; i--) {
+            digits[i] = digits[i] + 1;
+            if(digits[i] == 10) {
+                digits[i] = 0; 
+                // if it is first index, we need to expand the array.
+                // A case of 999 + 1 = 1000
+                if(i == 0) { 
+                    return arrayCopy(digits);
+                }
+            } else {
+                break;
+            }
+        }
+        return digits;
+    }
+    
+    private int[] arrayCopy(int[] digits) {
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+        for(int i = 0; i < digits.length; i++) {
+            result[i + 1] = digits[i];
+        }
+        return result;
+    }
+}
