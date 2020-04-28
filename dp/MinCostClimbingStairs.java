0224d02a48d1ce22cd2622da3c35dcb3976051ca
pankajwithgit <pankaj.davim@gmail.com>
2020-04-28 06:32:18 -0400

    https://leetcode.com/problems/min-cost-climbing-stairs/
    https://leetcode.com/problems/min-cost-climbing-stairs/

---
+++ dp/MinCostClimbingStairs.java
@@ -0,0 +1,15 @@
+class Solution {
+   // Bottom up computation - O(n) time, O(1) space
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int first = cost[0];
+        int second = cost[1];
+        if (n<=2) return Math.min(first, second);
+        for (int i=2; i<n; i++) {
+            int curr = cost[i] + Math.min(first, second);
+            first = second;
+            second = curr;
+        }
+        return Math.min(first, second);
+    }
+}
