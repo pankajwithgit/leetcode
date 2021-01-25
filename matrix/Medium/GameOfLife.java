232e74db0211dcba7af5c8389775f351abc25e6d
pankajwithgit <pankaj.davim@gmail.com>
2021-01-25 12:56:41 -0500

    Update GameOfLife.java

--- matrix/Medium/GameOfLife.java
+++ matrix/Medium/GameOfLife.java
@@ -5,7 +5,6 @@ Daily Coding Problem: Problem #39 [Medium]
 public class Solution {
     public void gameOfLife(int[][] board) {
         int m = board.length, n = board[0].length;
-        int[][] result = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 int countLive = 0;
